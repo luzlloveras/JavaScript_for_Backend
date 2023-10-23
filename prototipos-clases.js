06062867b85047de907c02fe951c32bd0f75ee59
luzlloveras <luzlloveras7@gmail.com>
2023-10-22 23:23:30 -0300

    add

--- prototipos-clases.js
+++ prototipos-clases.js
@@ -3,20 +3,43 @@ Class LikesService {
         this.likes = 0;
         this.dislikes = 0;
     }
+
+    likes() {
+        this.likes++;
+    }
+    
+    dislikes() {
+        this.dislikes++;
+    }
+    
+    getLikes() {
+        return this.likes;
+    }
+    
+    getDislikes() {
+        return this.dislikes;
+    }
 }
 
-likes() {
-    this.likes++;
+class Comment extends LikesService {
+    constructor(msg) {
+        super();
+        this.message = msg;
+    }
 }
 
-dislikes() {
-    this.dislikes++;
-}
+class CommentsService {
+    constructor() {
+        this.comments = [];
+        this.commentsQty = 0;
+    }
 
-getLikes() {
-    return this.likes;
-}
+    addComment(comment) {
+        this.commentsQty++;
+        this.comments.push({ id: this.commentsQty, comment});
+    }
 
-getDislikes() {
-    return this.dislikes;
+    getComments() {
+        return this.comments;
+    }
 }
